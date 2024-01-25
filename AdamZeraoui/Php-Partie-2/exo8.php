d83f06a27d6722f3b773f0f0c711565962ee5db2
AdamZeraoui <adam.zeraoui@hotmail.fr>
2024-01-25 11:15:19 +0100

    Start exo8

---
+++ AdamZeraoui/Php-Partie-2/exo8.php
@@ -0,0 +1,7 @@
+<h1>Exercice 8</h1>
+<p>Soit l’URL suivante : http://my.mobirise.com/data/userpic/764.jpg
+Créer une fonction personnalisée permettant d’afficher l’image N fois à l’écran.<br>
+Exemple :<br><code>
+repeterImage($url,4);</code>
+</p>
+<h2>Résultat</h2>
