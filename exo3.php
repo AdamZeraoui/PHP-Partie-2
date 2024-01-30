c191adf063de94e67cb8efe153114d4b7c07df29
AdamZeraoui <adam.zeraoui@hotmail.fr>
2024-01-30 18:01:26 +0100

    Supp

--- exo3.php
+++
@@ -1,8 +0,0 @@
-<h1>Exercice 3</h1>
-<p>Afficher un lien hypertexte permettant d'accéder au site d'Elan Formation. Le lien devra s'ouvrir dans un nouvelle onglet (_blank).</p>
-<h2>Résultat</h2>
-
-<?php
-
-$elan= "https://elan-formation.fr/accueil";
-echo "<a href='".$elan."'target='_blank'>Elan Formation</a>"; //target='_blank' sert à ouvrire le lien dans un nouvel onglet //
